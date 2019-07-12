17ac7d7d981a4a29808d36715da8781dcb56cd0a
steve75918 <steve75918@gmail.com>
2019-07-13 02:27:01 +0800

    [New, Test]Add unittest for module: board.board.

---
+++ gsf/tests/test_board_board.py
@@ -0,0 +1,51 @@
+import unittest
+from gsf.board.board import Board
+from gsf.board.tile import Tile
+from gsf.unit.unit import Unit
+
+class TestBoardBoard(unittest.TestCase):
+    def setUp(self):
+        self.board = Board(3,4)
+        pass
+
+    def test_getTile(self):
+        anytile = self.board.getTile(0, 1)
+        self.assertIsInstance(anytile, Tile)
+        pass
+
+    def test_addUnit(self):
+        anyUnit = Unit()
+        res = self.board.addUnit(anyUnit, 1, 1)
+
+        self.assertIsNone(res)
+        pass
+
+    def test_removeUnit(self):
+        anyUnit = Unit()
+        self.board.addUnit(anyUnit, 1, 1)
+
+        res= self.board.removeUnit(anyUnit, 1, 1)
+
+        self.assertIsNone(res)
+        pass
+
+    def test_removeUnits(self):
+        for i in range(3):
+            self.board.addUnit(Unit(), 1, 1)
+
+        res= self.board.removeUnits(1, 1)
+
+        self.assertIsNone(res)
+        pass
+
+    def test_getUnits(self):
+        for i in range(3):
+            self.board.addUnit(Unit(), 2, 1)
+
+        res = self.board.getUnits(2, 1)
+
+        self.assertIsInstance(res, list)
+        pass
+
+if __name__ == '__main__':
+    pass
