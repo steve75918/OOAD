5d4af577b05b7cb00a5c062a73e437b0fb1b812c
steve <steve75918@gmail.com>
2019-11-24 22:43:33 +0800

    [Modify] Change type of unitgroup, from list to group; Also make removeUnit() accept multiple type of unit to remove.

--- gsf/unit/unitgroup.py
+++ gsf/unit/unitgroup.py
@@ -1,26 +1,49 @@
 from gsf.unit.unit import Unit
 
+
 class UnitGroup:
-    def __init__(self):
-        self.units = []
+    def __init__(self, unitList: list):
+        self.units = {}
+
+        for unit in unitList:
+            units[unit.id] = unit
 
         pass
 
     def getUnitCounts(self):
         return len(self.units)
 
-    def addUnit(self, unit: Unit):
+    def addUnit(self, unit):
         if not isinstance(unit, Unit):
             raise TypeError('Please insert an Unit')
 
-        self.units.append(unit)
+        self.units[unit.id] = unit
 
         return True
 
-    def removeUnit(self, unit: Unit):
-        if not isinstance(unit, Unit):
-            raise TypeError('Please insert an Unit')
+    def getUnit(self, id: int):
+        if not isinstance(id, int):
+            TypeError('Not an id')
 
-        self.units.remove(unit)
+        return self.units[id]
+
+    def getUnits(self):
+        return self.units
+
+    def removeUnit(self, unit):
+        if isinstance(unit, Unit):
+            self.removeUnit(unit.id)
+        elif isinstance(unit, int):
+            self.remove(unit)
+        else:
+            raise TypeError('Unidentify unit with id')
+
+        return True
+
+    def remove(self, id: int):
+        if id not in self.units.keys():
+            raise TypeError('Invalid Id')
+
+        del self.units[id]
 
         return True
